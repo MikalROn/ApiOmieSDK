d3dac24ffb7d9c15ddb8a4fbe411fd328dd9eae6
micro <heromon.9010@gmail.com>
2023-02-13 06:34:25 -0300

    listar contas a pagar

--- omieapi/conta.py
+++ omieapi/conta.py
@@ -2,7 +2,7 @@ from omieapi.omie import Omie
 
 
 class Conta(Omie):
-    """ Classe que possui todos os metodos ralacionados a contas correntes no Omie """
+    """ Classe que possui todos os metodos ralacionados a contas no Omie """
     def _chamada_api_conta(self, call: str = '', param: dict | tuple | list = None) -> dict:
         """ Metodo feito para carregar o endpoint padrão da classe """
         return self._chamar_api(
@@ -246,6 +246,23 @@ class Conta(Omie):
             param={
                 "codigo_lancamento_omie": codigo_lancamento_omie,
                 "codigo_lancamento_integracao": codigo_lancamento_integracao
+            }
+        )
+
+    def listar_contas_pagar(self, pagina: int, registros_por_pagina: int, apenas_importado_api: bool) -> dict:
+        """
+        :param pagina:                              integer	Número da página que será listada.
+        :param registros_por_pagina:                integer	Número de registros retornados
+        :param apenas_importado_api:                bool	Exibir apenas os registros gerados pela API.
+        """
+        apenas_importado_api = self._bool_para_sn(apenas_importado_api)
+        return self._chamar_api(
+            call='ListarContasPagar',
+            endpoint='financas/contapagar/',
+            param={
+                "pagina": pagina,
+                "registros_por_pagina": registros_por_pagina,
+                "apenas_importado_api": apenas_importado_api
             }
         )
 
