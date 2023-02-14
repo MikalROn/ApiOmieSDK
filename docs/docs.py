0e24cb7b645b216ec4a7d306f354d436783cc8b4
micro <heromon.9010@gmail.com>
2023-02-14 01:07:56 -0300

    arrumando porojeto

--- docs/docs.py
+++ docs/docs.py
@@ -1,13 +1,14 @@
 import pydoc
 from flask import Flask
-from omieapi import Conta, Produtos, Omie
+from omieapi import Conta, Produtos, Omie, Geral
 
 app = Flask(__name__)
 
 doc = f'<h1 aling="center" > APi Omie  </h1>'\
       f'{pydoc.html.docclass(Omie)}' \
       f'{pydoc.html.docclass(Produtos)}' \
-      f'{pydoc.html.docclass(Conta)}'
+      f'{pydoc.html.docclass(Conta)}' \
+      f'{pydoc.html.docclass(Geral)}'
 
 @app.route('/')
 def index():
--- omieapi/__init__.py
+++ omieapi/__init__.py
@@ -1,2 +1,3 @@
 from omieapi.omie import Omie, Produtos
 from omieapi.conta import Conta
+from omieapi.geral import Geral
