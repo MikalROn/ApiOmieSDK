b59d3624fa0b55acf03a551c2c5c4d8efe6252d3
micro <heromon.9010@gmail.com>
2023-02-16 00:05:39 -0300

    Criando omie financas

---
+++ omieapi/omie_financas.py
@@ -0,0 +1,11 @@
+from omieapi import OmieBase
+from financas import *
+
+
+class Financas(OmieBase):
+    """ Classe que carrega todos os metodo relacionados a financias """
+    def Conta(self) -> Conta:
+        return Conta(self._appkey, self._appsecret)
+
+    def CupomFiscal(self) -> CupomFiscal:
+        return CupomFiscal(self._appkey, self._appsecret)
