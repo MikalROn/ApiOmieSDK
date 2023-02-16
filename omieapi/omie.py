7edaa447f217403d94c6fbda511304610f9d1ad3
micro <heromon.9010@gmail.com>
2023-02-16 00:05:28 -0300

    Criando omie

--- omieapi/omie.py
+++ omieapi/omie.py
@@ -1,5 +1,12 @@
 from omiebase import OmieBase
+from omie_geral import Geral
 
 
 class Omie(OmieBase):
-    """ """
+    """
+    Classe que carrega todos os metodos da api
+    :param omie_app_key:              Chave api da omie
+    :param omie_app_secret:           APi Secret da omie
+    """
+    def Geral(self) -> Geral:
+        return Geral(self._appkey, self._appsecret)
