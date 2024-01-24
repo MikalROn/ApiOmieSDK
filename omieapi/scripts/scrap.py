486442770c8bbb6276d461c031a8eb85d72c8567
daniel <heromon.9010@gmail.com>
2024-01-24 16:28:55 -0300

    feat: scrap tipos complexos

--- omieapi/scripts/scrap.py
+++ omieapi/scripts/scrap.py
@@ -36,27 +36,38 @@ def retorna_divs_de_metodos(url: str):
     soap = request_omieapi(url)
     return soap.find_all('div', {'class': 'methodItem'})
 
+def retorna_tipos_complexos(url: str):
+    def request_omieapi(url: str) -> BeautifulSoup:
+        try:
+            r = get(url, headers={'Content-type': 'text/html; charset=UTF-8'})
+            r.raise_for_status()
+            return BeautifulSoup(r.text, features="html.parser")
+        except:
+            raise Exception('Erro na requisição')
+    soap = request_omieapi(url)
+    return soap.find_all('div', {'class': 'complexTypeItem'})
+    
+    
 
 def pegar_todos_os_metodos(urls: list[str]):
+    
+    def pegar_descricao(item) -> str | None:
+        try:
+            return item.p.text.strip()
+        except:
+            return None
+                
+    def pegar_retorno(item):
+        try:
+            return item.find('span', {'class': 'lightBlue'}).text
+        except:
+            return None
+        
     acerto_erro = [0, 0]
     dicionario_final = {}
     for url in urls:
         for item in retorna_divs_de_metodos(url):
-            
-            def pegar_descricao(item) -> str | None:
-                try:
-                    return item.p.text.strip()
-                except:
-                    return None
-                
-            def pegar_retorno(item):
-                try:
-                    return item.find('span', {'class': 'lightBlue'}).text
-                except:
-                    return None
-                
             try:
-                
                 metodo = item.h3.text.strip()
                 descricao = pegar_descricao(item) if pegar_descricao(item) else ""
                 retorno = pegar_retorno(item) if pegar_retorno(item) else ""
@@ -85,6 +96,8 @@ def pegar_todos_os_metodos(urls: list[str]):
 def metodos():
     return pegar_todos_os_metodos([x for x in pega_links_api().values()])
 
+def tipos():
+    return pega_todos_tipos_complexos([x for x in pega_links_api().values()])
 
 def constroe_parametro(parametros: dict):
     codigo = ''
@@ -139,6 +152,60 @@ def gerar_codigo_automatico(dicionario: dict):
 def gerar_codigo(metodos) -> None:
     gerar_codigo_automatico(metodos)
 
+def gerar_codigo_tipos_automatico_em_python(lista_tipos: list):
+    codigo = 'from dataclasses import dataclass \n\n' \
+             '# Aviso -> antes de usar confira se não a oq vc precisa já feito no codigo principal,\n' \
+             '# o codigo autogerdo pode conter erros não detectados ainda\n\n'
+    for tipo in lista_tipos:
+        codigo += f'''@dataclass\nclass {tipo["nome"]}:\n    """{tipo["descricao"]}"""\n'''
+    
+        for parametro in tipo['parametros']:
+            codigo += f'    {parametro["nome_parametro"]} #{parametro["tipo_parametro"]}\n'
+        
+    with open('tipos_complexos.py', 'w', encoding='utf-8') as w:
+        w.write(codigo)
+
+
+def pega_todos_tipos_complexos(urls: list[str]) -> list:
+    
+    remove_numero = lambda x: ''.join([i for i in x if not i.isdigit()])
+    
+    lista_tipos: list = []
+    
+    for url in urls:
+        for item in retorna_tipos_complexos(url):
+            parametros = []
+            try:
+                nome = item.h3.text
+                descricao = item.p.text
+
+                for parametro in item.table.find_all("tr"):
+                    
+                    nome_parametro = parametro.find("td", {"class": "parameter-name"})
+                    tipo = parametro.find("td", {"class": "parameter-type"})
+                    descricao_parametro = parametro.find("td", {"class": "parameter-docs"})
+                    parametros.append(
+                        {
+                            "nome_parametro": nome_parametro.text if nome_parametro else "",
+                            "tipo_parametro": remove_numero(tipo.text) if tipo else "None",
+                            "descricao_parametro": descricao_parametro.text if descricao_parametro else ""
+                        }
+                    )
+                    
+                lista_tipos.append(
+                    {
+                        "nome": nome,
+                        "descricao": descricao,
+                        "parametros": parametros
+                    }
+                )
+
+            except Exception as error:
+                print(error)
+    print("Fim acabou")
+    print(lista_tipos)
+    return lista_tipos
 
 if __name__ == '__main__':
-    gerar_codigo(metodos())
+    #gerar_codigo(metodos())
+    gerar_codigo_tipos_automatico_em_python(tipos())
