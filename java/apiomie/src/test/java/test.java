a09e982ebb388e2b9362ac705abfb8bac8458be6
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2024-04-27 15:31:46 -0300

    testando api em java

--- java/apiomie/src/test/java/test.java
+++ java/apiomie/src/test/java/test.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class test {
-
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
         Omie app = OmieApi.gerarOmieHomologacao();
         JSONObject produtos = app.listarProdutos(
-                new JSONArray().put(new JSONObject().put("pagina", 1))
+                new JSONArray().put(new JSONObject()
+                        .put("pagina", 1)
+                        .put("registros_por_pagina", 50)
+                        .put("filtrar_apenas_omiepdv", "N")
+                )
         );
         System.out.println(produtos.toString());
     }
