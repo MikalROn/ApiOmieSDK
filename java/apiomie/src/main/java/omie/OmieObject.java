a81b1acbf575973d430ecea95e0218c33ccae921
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2024-10-19 13:14:14 -0300

    add: javadcs

--- java/apiomie/src/main/java/omie/OmieObject.java
+++ java/apiomie/src/main/java/omie/OmieObject.java
@@ -10,6 +10,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * The OmieObject class handles API communication with the Omie API.
+ * It uses HttpClient to send HTTP requests and parse responses in JSON format.
+ */
+
 public  class OmieObject {
 
     private String url;
@@ -17,12 +22,30 @@ public  class OmieObject {
     private String appSecreet;
     private HttpClient client;
 
+    /**
+     * Constructor to initialize the OmieObject with appKey and appSecret.
+     * 
+     * @param appKey The application key for Omie API authentication.
+     * @param appSecret The application secret for Omie API authentication.
+     */
+
     public OmieObject(String appKey, String appSecreet){
         this.url = "https://app.omie.com.br/api/v1/";
         this.client = HttpClient.newHttpClient();
         this.appKey = appKey;
         this.appSecreet = appSecreet;
     }
+    /**
+     * Sends a request to the Omie API and returns a JSON response.
+     * 
+     * @param endpoint The API endpoint to connect to.
+     * @param call The method being called in the Omie API.
+     * @param parametros The parameters for the API call in a JSONArray format.
+     * @return A JSONObject with the API response.
+     * @throws URISyntaxException If the URI syntax is invalid.
+     * @throws IOException If an I/O error occurs during the request.
+     * @throws InterruptedException If the request is interrupted.
+     */
 
     protected JSONObject chamarApi(String endpoint, String call, JSONArray parametros) throws URISyntaxException, IOException, InterruptedException {
         // Conecta com api e devolve um JSON
@@ -40,6 +63,18 @@ public  class OmieObject {
         return new JSONObject(response.body());
     }
 
+    /**
+     * A public method that allows API requests with the specified endpoint, call, and parameters.
+     * 
+     * @param endpoint The API endpoint to connect to.
+     * @param call The method being called in the Omie API.
+     * @param parametros The parameters for the API call in a JSONArray format.
+     * @return A JSONObject with the API response.
+     * @throws URISyntaxException If the URI syntax is invalid.
+     * @throws IOException If an I/O error occurs during the request.
+     * @throws InterruptedException If the request is interrupted.
+     */
+
     public JSONObject concatarApi(String endpoint, String call, JSONArray parametros) throws URISyntaxException, IOException, InterruptedException{
         return this.chamarApi(
                 endpoint,
@@ -47,6 +82,14 @@ public  class OmieObject {
                 parametros
         );
     }
+
+    /**
+     * Generates a JSON string for the API request body.
+     * 
+     * @param call The method being called in the Omie API.
+     * @param parametros The parameters for the API call in a JSONArray format.
+     * @return A string containing the JSON request body.
+     */
 
     private String gerarJson(String call, JSONArray parametros) {
         return new JSONObject()
