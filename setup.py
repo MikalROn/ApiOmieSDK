85493a893b3138ad35cb85974f218eb4eadeab17
Coelho <109885900+MikalROn@users.noreply.github.com>
2025-06-11 12:54:37 -0300

    Update setup.py 0.4.61

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 from setuptools import setup, find_packages
 
-VERSION =  '0.4.6'
+VERSION =  '0.4.61'
 
 
 with open('README.md', 'rt', encoding='utf-8') as arq:
       readme = arq.read()
 
 keywords = [
-    'Omie API', 'API Omie', 'Omie integração', 'Omie SDK', 'SDK para Omie', 
-    'integração Omie', 'Omie API SDK', 'omieapi', 'apiomiesdk', 'omie sdk', 
-    'automatização Omie', 'conexão Omie', 'desenvolvimento Omie', 
-    'Omie Python', 'API ERP Omie', 'Omie integração Python', 'Omie API integração',
-    'API para Omie', 'Omie API integração'
+    'Omie API',
+    'Omie SDK',
+    'Omie integração',
+    'Omie Python',
+    'API Omie SDK',
+    'Omie ERP',
+    'Omie automação',
+    'Python Omie',
+    'Omie API integração',
+    'Omie developer',
+    'Omie finanças',
+    'Omie CRM',
+    'Python ERP integração',
+    'Omie Brasil',
+    'ERP integração Python'
+]
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Office/Business :: Financial :: Accounting"
 ]
 
 setup(name='api-omie',
@@ -23,10 +47,8 @@ setup(name='api-omie',
       long_description_content_type='text/markdown',
       author_email='heromon.9010@gmail.com',
       keywords=keywords,
-      classifiers=[
-            "Development Status :: 3 - Alpha"
-      ],
-      description='Ferramenta para api do omie não oficial',
+      classifiers=classifiers,
+      description='SDK em Python para integração não oficial com a API do Omie ERP',
       packages=find_packages(exclude=('omieapi/scripts/scrap.py', 'java/*', 'php/*')),
       install_requires=['requests', 'beautifulsoup4', 'pandas', 'httpx'],
       python_requires='>=3',
